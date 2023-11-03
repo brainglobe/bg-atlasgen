29343dc6388c57150de8c965a1b68f895573da1c
viktorpm <plattnerviktor@gmail.com>
2023-11-03 15:38:36 +0000

    fix linting

--- tests/test_unit/test_validation.py
+++ tests/test_unit/test_validation.py
@@ -1,22 +1,36 @@
 from pathlib import Path
+
 import numpy as np
 import pytest
 from bg_atlasapi import BrainGlobeAtlas
 from bg_atlasapi.config import get_brainglobe_dir
 
-from bg_atlasgen.validate_atlases import validate_atlas_files, _assert_close, validate_mesh_matches_image_extents
+from bg_atlasgen.validate_atlases import (
+    _assert_close,
+    validate_atlas_files,
+    validate_mesh_matches_image_extents,
+)
 
 
 def test_validate_mesh_matches_image_extents():
     atlas = BrainGlobeAtlas("allen_mouse_100um")
     assert validate_mesh_matches_image_extents(atlas)
 
+
 def test_validate_mesh_matches_image_extents_negative(mocker):
     atlas = BrainGlobeAtlas("allen_mouse_100um")
     flipped_annotation_image = np.transpose(atlas.annotation)
-    annotation_mock = mocker.patch("bg_atlasapi.BrainGlobeAtlas.annotation", new_callable=mocker.PropertyMock, return_value=flipped_annotation_image)
-    with pytest.raises(AssertionError, match="differ by more than 10 times pixel size"):
+    mocker.patch(
+        "bg_atlasapi.BrainGlobeAtlas.annotation",
+        new_callable=mocker.PropertyMock,
+        return_value=flipped_annotation_image,
+    )
+    with pytest.raises(
+        AssertionError, match="differ by more than 10 times pixel size"
+    ):
         validate_mesh_matches_image_extents(atlas)
+
+
 def test_valid_atlas_files():
     _ = BrainGlobeAtlas("allen_mouse_100um")
     atlas_path = Path(get_brainglobe_dir()) / "allen_mouse_100um_v1.2"
@@ -32,6 +46,9 @@ def test_invalid_atlas_path():
 def test_assert_close():
     assert _assert_close(99.5, 8, 10)
 
-def  test_assert_close_negative():
-    with pytest.raises(AssertionError, match="differ by more than 10 times pixel size"):
+
+def test_assert_close_negative():
+    with pytest.raises(
+        AssertionError, match="differ by more than 10 times pixel size"
+    ):
         _assert_close(99.5, 30, 2)
